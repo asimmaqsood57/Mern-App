3b1783b0d2b057be2e54c12f397440470fcd15a9
asimmaqsood57 <asimmaqsood57@gmail.com>
2021-08-08 08:49:50 +0500

    record inserted in dataabase successfully

--- server/index.js
+++ server/index.js
@@ -1,1 +1,42 @@
 const express = require("express");
+
+//cP8Fj8XVI8UIsXqv
+
+//mongodb+srv://new-user_31:<password>@mern-app.ztkdk.mongodb.net/test
+const mongoose = require("mongoose");
+
+const FoodModel = require("./models/Food");
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://new-user_31:cP8Fj8XVI8UIsXqv@mern-app.ztkdk.mongodb.net/food?retryWrites=true&w=majority",
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => {
+    console.log("database connection successful");
+  });
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  const food = new FoodModel({
+    foodName: "Apple",
+    daysSinceIAte: 3,
+  });
+
+  try {
+    await food.save().then((result) => {
+      console.log(result);
+      res.send("record inserted");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(3001, () => {
+  console.log("server running at port 3001");
+});
---
+++ server/models/Food.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const foodSchema = new mongoose.Schema({
+  foodName: {
+    type: String,
+    required: true,
+  },
+  daysSinceIAte: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Food = mongoose.model("foodData", foodSchema);
+
+module.exports = Food;
