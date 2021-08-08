8e679c21113bbd7b99e0ccfde3e3b1a330d2cf40
asimmaqsood57 <asimmaqsood57@gmail.com>
2021-08-08 11:23:36 +0500

    Read operation done

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import Axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+// import { response } from "express";
 
 function App() {
   const [foodName, setfoodName] = useState("");
   const [days, setdays] = useState(0);
+
+  const [foodList, setfoodList] = useState([]);
   const addToList = () => {
     // console.log(foodName, days);
     Axios.post("http://localhost:3001/insert", {
@@ -13,6 +16,13 @@ function App() {
       days: days,
     });
   };
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/read").then((response) => {
+      // console.log(response);
+      setfoodList(response.data);
+    });
+  }, []);
   return (
     <div className="App">
       <h1>CRUD App with MERN</h1>
@@ -37,6 +47,16 @@ function App() {
       />
 
       <button onClick={addToList}>ADD to List</button>
+
+      <h1>Food List</h1>
+
+      {foodList.map((val, key) => {
+        return (
+          <div key={key}>
+            <h1>{val.foodName}</h1>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -43,6 +43,15 @@ app.post("/insert", async (req, res) => {
   }
 });
 
+app.get("/read", async (req, res) => {
+  FoodModel.find({}, (err, result) => {
+    if (err) {
+      res.send(err);
+    }
+    res.send(result);
+  });
+});
+
 app.listen(3001, () => {
   console.log("server running at port 3001");
 });
